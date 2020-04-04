8eaff2382ccf78d21f84fe344107b858b8c13704
Rakesh-S <s.rakesh@outlook.in>
2020-04-04 15:01:59 +0530

    First commit to this repository

---
+++ hello_1.c
@@ -0,0 +1,22 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+MODULE_LICENSE("GPL");
+
+MODULE_AUTHOR("RAKESH");
+
+MODULE_VERSION("0.1");
+
+static int __init hello_start(void)
+{
+	printk(KERN_INFO "Hello world\n");
+	return 0;
+}
+
+static void __exit hello_exit(void)
+{
+	printk(KERN_INFO "Good bye\n");
+}
+
+module_init(hello_start);
+module_exit(hello_exit);
